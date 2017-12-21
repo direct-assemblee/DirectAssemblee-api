b838fad6eb3ea73837776f8364770cc34a8d94f6
Morgane Plat <morgane.plat@backelite.com>
2017-12-21 23:49:50 +0100

    Adding logs for extra infos

--- api/services/ExtraInfoService.js
+++ api/services/ExtraInfoService.js
@@ -1,6 +1,11 @@
 module.exports = {
     findExtraInfosForWork: function(workId) {
+        console.time('      findExtraInfosForWork for ' + workId);
         return ExtraInfo.find()
         .where({ workId: workId })
+        .then(function(work) {
+            console.timeEnd('      findExtraInfosForWork for ' + workId);
+            return work
+        })
     }
 }
