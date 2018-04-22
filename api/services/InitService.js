cdd9d5022cdf2821061cc4fee71deaed7c53e509
Morgane Plat <morgane.plat@backelite.com>
2018-04-22 20:59:53 +0200

    adding redis

--- api/services/InitService.js
+++ api/services/InitService.js
@@ -2,9 +2,11 @@ let ThemeHelper = require('./helpers/ThemeHelper')
 
 module.exports = {
     init: function() {
-        ThemeHelper.initThemes();
-        PushNotifService.startDailyVotesCron();
-        ActivityRateService.startUpdateCron();
-        LastWorksService.initLastScanTime();
+        if (process.env.NODE_APP_INSTANCE === '0') {
+            ThemeHelper.initThemes();
+            PushNotifService.startDailyVotesCron();
+            ActivityRateService.startUpdateCron();
+            LastWorksService.initLastScanTime();
+        }
     }
 }
