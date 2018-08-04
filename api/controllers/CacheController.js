b5ecd0e8229554bea92352b22708ee958039f2de
Morgane Plat <morgane.plat@backelite.com>
2018-08-04 16:40:46 +0200

    fixed resetCache bug

--- api/controllers/CacheController.js
+++ api/controllers/CacheController.js
@@ -2,9 +2,7 @@ let CacheService = require('../services/CacheService');
 
 module.exports = {
 	resetCache: function(req, res) {
-		return CacheService.resetAll()
-		.then(function() {
-			return res.status(200).json('OK');
-		})
+		CacheService.resetAll()
+		return res.status(200).json('OK');
 	}
 };
